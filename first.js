cf93eac2039d8dfa4d84b23a6a950385701e1aaf
Bhaskar Mishra <bhaskar.mishra9118@gmail.com>
2026-03-15 20:52:02 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,33 @@
+const text = ["MERN Stack Developer", "Full Stack Developer", "Backend Developer"];
+
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+
+currentText = text[i];
+
+if(!isDeleting){
+document.getElementById("typing").textContent = currentText.substring(0,j++);
+if(j > currentText.length){
+isDeleting = true;
+setTimeout(type,1000);
+return;
+}
+}
+
+else{
+document.getElementById("typing").textContent = currentText.substring(0,j--);
+if(j < 0){
+isDeleting = false;
+i = (i+1) % text.length;
+}
+}
+
+setTimeout(type,120);
+}
+
+type();
+
